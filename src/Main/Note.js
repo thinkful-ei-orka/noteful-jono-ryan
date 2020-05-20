6ef499dd84fb98a8b9a78aa4aa4a33732bd8c207
Orkun Ozekin <orkunozekin@thinkful.com>
2020-05-19 20:44:06 -0500

    adjusted time formatting

--- src/Main/Note.js
+++ src/Main/Note.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-// import { format } from 'date-fns'
+import moment from 'moment';
+
 
 export default function Note(props) {
     return(
@@ -8,7 +9,7 @@ export default function Note(props) {
             {props.notes.map(note => 
             <div className='noteItem' key={note.id} >
             <NavLink className='navlink note-name' to={'/note/' + note.id}>{note.name}</NavLink>
-            <p className='date'>{`Date modified on ${note.modified}`}</p>
+            <p className='date'>{`Date modified on ${moment(note.modified).format('MMM D YYYY')}`}</p>
             <button className='delete-note'>Delete Note</button>
             {props.showDescription ? (<p>{note.content}</p>): ''}
             </div>
